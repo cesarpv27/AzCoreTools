f4c4dc24abdcd06f779e1e6f98eb611feca2b179
Cesar PV <cesar.pv27@gmail.com>
2021-07-13 23:49:58 -0400

    TestEnvironment update with new features
    Nuget version: 1.3.4-beta

--- AzCoreTools/Utilities/TestEnvironment.cs
+++ AzCoreTools/Utilities/TestEnvironment.cs
@@ -11,16 +11,89 @@ namespace AzCoreTools.Utilities
 {
     public sealed class TestEnvironment
     {
-        private const string StorageUriCustomKey = "StorageUri";
-        private const string PrimaryStorageAccountKeyCustomKey = "PrimaryStorageAccountKey";
-        private const string StorageAccountNameCustomKey = "StorageAccountName";
-        private const string StorageConnectionStringCustomKey = "StorageConnectionString";
+        public const string StorageUriCustomKey = "StorageUri";
+        public const string PrimaryStorageAccountKeyCustomKey = "PrimaryStorageAccountKey";
+        public const string StorageAccountNameCustomKey = "StorageAccountName";
+        public const string StorageConnectionStringCustomKey = "StorageConnectionString";
+        public const string CosmosEndpointUriCustomKey = "CosmosEndpointUri";
+        public const string CosmosPrimaryKeyCustomKey = "CosmosPrimaryKey";
 
-        private static string _environmentVariablesFilePath = 
-            Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), "_environmentVariablesInfo.json");
-        public static void SetEnvironmentVariablesFilePath(string newEnvironmentVariablesFilePath)
+        public static void CreateAndSaveEnvironmentVariablesFile(string ConnectionString, string filePath)
         {
-            _environmentVariablesFilePath = newEnvironmentVariablesFilePath;
+            var dict = new Dictionary<string, string>();
+            dict.Add(StorageConnectionStringCustomKey, ConnectionString);
+
+            CreateAndSaveEnvironmentVariablesFile(dict, filePath);
+        }
+
+        public static void CreateAndSaveEnvironmentVariablesFile(
+            string ConnectionString, 
+            string CosmosEndpointUri, 
+            string CosmosPrimaryKey, 
+            string filePath)
+        {
+            var dict = new Dictionary<string, string>();
+            dict.Add(StorageConnectionStringCustomKey, ConnectionString);
+            dict.Add(CosmosEndpointUriCustomKey, CosmosEndpointUri);
+            dict.Add(CosmosPrimaryKeyCustomKey, CosmosPrimaryKey);
+
+            CreateAndSaveEnvironmentVariablesFile(dict, filePath);
+        }
+
+        public static void CreateAndSaveEnvironmentVariablesFile(Dictionary<string, string> dict, string filePath)
+        {
+            string privateKey = GenerateNaivePersonalPrivateKey();
+
+            var serializedDict = JsonConvert.SerializeObject(dict);
+            File.WriteAllText(filePath, AesCrypto.Encrypt(serializedDict, privateKey));
+        }
+
+        public static string LoadEnvironmentVariablesFile(string filePath)
+        {
+            return AesCrypto.Decrypt(File.ReadAllText(filePath), GenerateNaivePersonalPrivateKey());
+        }
+
+        private static string _environmentVariablesFilePath;
+        public static string EnvironmentVariablesFilePath
+        {
+            get 
+            {
+                if (string.IsNullOrEmpty(_environmentVariablesFilePath))
+                    _environmentVariablesFilePath = GetDefaultEnvironmentVariablesFilePath();
+
+                return _environmentVariablesFilePath;
+            }
+            internal set
+            {
+                if (string.IsNullOrEmpty(value) || string.IsNullOrWhiteSpace(value))
+                    throw new ArgumentNullException("path is null or empty or whitespace");
+                if (Path.IsPathRooted(value))
+                    throw new ArgumentException("path is not rooted");
+                if (File.Exists(value))
+                    throw new ArgumentException("file already exists");
+                if (value.EndsWith("/"))
+                    throw new ArgumentException("path is not well formed");
+
+                _environmentVariablesFilePath = value;
+            }
+        }
+
+        public static string GetDefaultEnvironmentVariablesFilePath()
+        {
+            return Path.Combine(GetDefaultEnvironmentVariablesFolderPath(), "_environmentVariablesInfo.json");
+        }
+
+        private static string GetDefaultEnvironmentVariablesFolderPath()
+        {
+            return Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+        }
+
+        public static void SetEnvironmentVariablesFileName(string newFileName)
+        {
+            if (string.IsNullOrEmpty(newFileName) || string.IsNullOrWhiteSpace(newFileName))
+                throw new ArgumentNullException("file name is null or empty or whitespace");
+
+            EnvironmentVariablesFilePath = Path.Combine(GetDefaultEnvironmentVariablesFolderPath(), newFileName);
         }
 
         private static Dictionary<string, string> _environmentVariables;
@@ -30,11 +103,11 @@ namespace AzCoreTools.Utilities
             {
                 if (_environmentVariables == null)
                 {
-                    if (!File.Exists(_environmentVariablesFilePath))
+                    if (!File.Exists(EnvironmentVariablesFilePath))
                         throw new ApplicationException("Environment variables file path not found");
 
                     _environmentVariables = JsonConvert.DeserializeObject<Dictionary<string, string>>(
-                        AesCrypto.Decrypt(File.ReadAllText(_environmentVariablesFilePath), GenerateNaivePersonalPrivateKey()));
+                        LoadEnvironmentVariablesFile(EnvironmentVariablesFilePath));
                     if (_environmentVariables == null)
                         throw new ApplicationException("Environment variables not found");
                 }
@@ -76,6 +149,16 @@ namespace AzCoreTools.Utilities
         public static bool TryGetStorageConnectionString(out string value)
         {
             return TryGetValue(StorageConnectionStringCustomKey, out value);
+        }
+
+        public static bool TryGetCosmosEndpointUri(out string value)
+        {
+            return TryGetValue(CosmosEndpointUriCustomKey, out value);
+        }
+        
+        public static bool TryGetCosmosPrimaryKey(out string value)
+        {
+            return TryGetValue(CosmosPrimaryKeyCustomKey, out value);
         }
 
         #endregion
